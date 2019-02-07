bf0011e595a0f1c9f4ae4accacd855e9164bb21e
Kevin Ries <kevinries@Kevins-iMac-Pro.fritz.box>
2019-02-07 19:35:26 +0100

    Added setup file for Jest to delete *.db files after running tests

---
+++ packages/db/test/setup.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const path = require('path')
+
+// delete db files after testing
+afterAll(() => {
+    try {
+        const files = ['test.db', 'threads.db', 'users.db']
+        files.forEach(file => fs.unlink(path.join(__dirname, '..', file), () => {}))
+    } catch (e) {}
+})
